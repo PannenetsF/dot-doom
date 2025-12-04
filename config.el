901ab466fdd456724ad4e1e7090656a4b50c2304
fanyunqian.1 <fanyunqian.1@bytedance.com>
2025-12-04 23:43:03 +0800

    feat(ui): change the theme

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 18 ))
+(setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 18 ))
 (setq use-default-font-for-symbols nil)
 
 (defun private-cjk-font()
@@ -40,7 +40,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-ayu-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
